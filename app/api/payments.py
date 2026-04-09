e0ad1b5d3e4df6aac00b10f9836928e901792892
mritunjay <mritunjaypandey0789@gmail.com>
2026-04-09 21:02:08 +0530

    Import issue fix

--- app/api/payments.py
+++ app/api/payments.py
@@ -5,7 +5,7 @@ import hmac
 import hashlib
 
 from app.core.database import get_db
-from app.core.security import get_current_user
+from app.api.auth import get_current_user
 from app.core.config import settings
 from app.models.user import User
 from app.models.payment import Transaction
